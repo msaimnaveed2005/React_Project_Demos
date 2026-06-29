67997a1580aedb13292edae20be4eaae70063c06
Muhammad Saim Naveed <msaimnaveed5384@gmail.com>
2026-06-29 15:31:53 +0500

    Complete Project 3: Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Todo from './Components/Todo'
+import Calculator from './Components/Calculator'
+// import Todo from './Components/Todo'
 // import Counter from './Components/Counter'
 
 const App = () => {
   return (
     <div>
       {/* <Counter/> */}
-      <Todo/>
+      {/* <Todo/> */}
+      <Calculator/>
     </div>
   )
 }
